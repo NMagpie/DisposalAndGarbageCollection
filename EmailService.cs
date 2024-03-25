d46dea37d879c89b5474505cb8d3eeb26553cd2a
NMagpie <sorochin.work@gmail.com>
2024-03-25 15:31:27 +0200

    ASSIGNMENT COMPLETED
     - Implemented EmailService using built-in credentials.
    
     - Implemented Resources Disposal after being used.

---
+++ EmailService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+
+namespace _10._Disposal_and_Garbage_collection
+{
+    public class EmailService : IDisposable
+    {
+        private bool _disposedValue;
+
+        public SmtpClient Client { get; init; }
+
+        public string Username { get; init; }
+
+        public EmailService()
+        {
+            //Creating configuration, retrieving uname and pass for using SMTP gmail server.
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+                .Build();
+
+            Username = config.GetSection("credentials")["username"] ?? "";
+
+            var pass = config.GetSection("credentials")["pass"];
+
+            Client = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(Username, pass),
+                EnableSsl = true,
+            };
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    Client.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using _10._Disposal_and_Garbage_collection;
+
+Console.WriteLine("Subscribing the newsletter. To exit applicaiton type \"q\"");
+
+var topic = "You were subscribed successfully!";
+
+var body = "We thank you for subscribing to out newsletter!";
+
+var consoleInpt = "";
+
+using (var service = new EmailService())
+{
+    var username = service.Username;
+
+    while (true)
+    {
+        Console.WriteLine("Enter your email address:");
+
+        consoleInpt = Console.ReadLine();
+
+        if (consoleInpt.Equals("q", StringComparison.CurrentCultureIgnoreCase))
+            break;
+
+        try
+        {
+            service.Client.Send(username, consoleInpt, topic, body);
+            Console.WriteLine("The address has been subsribed successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
